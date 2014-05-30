0f8234a8b98ebe7f14902876b26bab82ca062b49
Spencer Lanyon <spencer.lanyon@gmail.com>
2014-05-30 12:27:33 -0700

    Added support for recommender to send back json

--- app/controllers/command_recommender_controller.rb
+++ app/controllers/command_recommender_controller.rb
@@ -5,30 +5,56 @@ class CommandRecommenderController < ApplicationController
 	respond_to :json
 
 	def upload_data
-		File.open('/home/spencer/test_file.json', 'w'){|f| 
-			f.write(params.inspect)
-		}
-		save_data(params[:user_id],params[:commands])
-		render text: params[:usage_data]		
+		saved = save_data(params[:usage_data][:user_id],params[:usage_data][:data])
+		respond_to do |format|
+			if saved
+				format.json {render :json => params[:usage_data]}
+			else
+				format.json {render json: "Somethign wrong", status: 422}
+			end	
+		end
 	end
 
-	def get_recommendations
-		recommendation = Recommendation.new(:user_id)
-		render text: recommendation.create_recommendation()
+	def get_recommendations	
+		recommendation = get_recommendation(params[:usage_data][:user_id])
+		render text: recommendation
+	end
+
+	def test_rec
+		recommendation = get_recommendation("abcde")
+		render text: recommendation
 	end
 
 	private
 	def save_data(user_id, usage_data)
-			usage_data.each{ |command|
-			new_command = Command.new(:user_id => user_id,
+			saved = true
+			usage_data.each{ |command|	
+				new_command = Command.new(:user_id => user_id,
 										:what => command[:what],
 										:kind => command[:kind],
-										:bundlVersion => command[:bundleVersion],
+										:bundleVersion => command[:bundleVersion],
 										:description => command[:description],
 										:bindingUsed => command[:bindingUsed],
 										:time => command[:time])
-			new_command.save!
+				logger.debug new_command.inspect
+						
+				if not new_command.save
+					saved = false
+				end
 										
-		}		
+			}
+			return saved		
+	end
+
+
+	def get_recommendation(user_id)
+		jar_location = Rails.root.join('lib')
+		
+		command = "java -jar #{Rails.root}/lib/recommender.jar 1 " + user_id
+		puts command
+		recommend = IO.popen(command)	
+				
+		return recommend.gets
 	end
 end
+
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,19 +1,9 @@
 class Recommendation
-
-	def initialize(user_id)
-		@user_id = user_id
-	end
+	attr_access :what, :kind, :description
 	
-	def create_recommendation()
-	  recommendations = get_recommendation(@user_id)
-	  return recommendations
+	def initialize(what, kind, description)
+		@what = what
+		@kind = kind
+		@description = description
 	end
-
-private
-	def get_recommendation(user_id)
-		recommend = IO.popen("java -jar ~/ruby_test/dummy.jar 5")	
-		#recommend = IO.popen("date")	
-		return recommend.gets
-	end
-  		  
 end
